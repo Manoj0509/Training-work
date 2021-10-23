bb10aa7fb5096f21dec9be74de7190acfc9213f8
MANOJ KUMAR THAKUR <92718292+Manoj0509@users.noreply.github.com>
2021-10-23 11:26:29 +0530

    Add files via upload

---
+++ CapgeminiLabWork/src/exceptionhandling/EmplyoeeException.java
@@ -0,0 +1,16 @@
+package exceptionhandling;
+
+
+	
+
+	public class EmplyoeeException extends Exception{
+		
+		
+		public EmplyoeeException(String msg) {
+			super(msg);
+			// TODO Auto-generated constructor stub
+		}
+
+		
+
+}
---
+++ CapgeminiLabWork/src/lab4exercise3/LibInheriLab4.java
@@ -0,0 +1,12 @@
+package lab4exercise3;
+
+public class LibInheriLab4 {
+
+
+		public static void main(String[] args) {
+			
+		
+	}
+	
+
+}
