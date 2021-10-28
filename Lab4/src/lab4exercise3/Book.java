a8adfcf90a76f235fd092c209d95cff351ea9a9d
MANOJ KUMAR THAKUR <92718292+Manoj0509@users.noreply.github.com>
2021-10-28 16:32:06 +0530

    Add files via upload

---
+++ Lab4/src/lab4exercise3/Book.java
@@ -0,0 +1,20 @@
+package lab4exercise3;
+class Book extends WrittenItem {
+
+	
+	public Book(int idn, String title, int noOfCopies, String author) {
+		super(idn, title, noOfCopies, author);
+	}
+
+	
+	public void getBooks() {
+
+	}
+	
+	public void addBook(Book b) {
+
+	}
+}
+
+
+
