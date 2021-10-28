9ff543ed91b1763d79cc0f6468ad3b48629cc3e1
MANOJ KUMAR THAKUR <92718292+Manoj0509@users.noreply.github.com>
2021-10-28 14:44:37 +0530

    Add files via upload

---
+++ Lab6/src/collection/CountChar.java
@@ -0,0 +1,27 @@
+package collection;
+
+import java.io.*;
+import java.util.*;
+public class CountChar{
+   static void count_characters(String input_str){
+      HashMap<Character, Integer> my_map = new HashMap<Character, Integer>();
+      char[] str_array = input_str.toCharArray();
+      for (char c : str_array){
+         if (my_map.containsKey(c)){
+            my_map.put(c, my_map.get(c) + 1);
+         }else{
+            my_map.put(c, 1);
+         }
+      }
+      for (Map.Entry entry : my_map.entrySet()){
+         System.out.println(entry.getKey() + " " + entry.getValue());
+      }
+   }
+   public static void main(String[] args){
+	   System.out.println("enter the characters");
+	   Scanner sc =new Scanner(System.in);
+      String my_str=sc.next();
+      System.out.println("The occurence of every character in the string is ");
+      count_characters(my_str);
+   }
+}
